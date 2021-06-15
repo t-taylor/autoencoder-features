df74e06e908d06a593fc0afc9ab4aa00dbe3ce75
Tom Taylor <ttaylor3.142@gmail.com>
2021-06-15 10:25:33 +0100

    Add gitignore

--- part1.py
+++ part1.py
@@ -8,11 +8,13 @@ from scipy.io import arff
 import pandas as pd
 
 def main():
-    print('Test')
+    nsl()
 
 ### NSL-KDD
 def nsl():
-    nsldata = arff.loadarff('../data/nsl/KDDTrain+.arff')
+    nsldata = arff.loadarff('data/nsl/KDDTrain+.arff')
+    nsldf = pd.DataFrame(nsldata[0])
+    print(nsldf.columns)
 
 ### Microsoft Malware
 def malware():
