8f8b8a5680e46ded84acb8c567f2dd17a1ce9abe
Tom Taylor <ttaylor3.142@gmail.com>
2021-06-21 11:51:45 +0100

    Able to read in asm and output opt

--- part1.py
+++ part1.py
@@ -4,21 +4,29 @@
 # * Normalisation and Standardization
 # * Training & testing without autoencoder (?)
 
+from pyparsing import Word, hexnums, WordEnd, Optional, alphas, alphanums
 from scipy.io import arff
 import pandas as pd
 
 def main():
-    nsl()
+    malware()
 
 ### NSL-KDD
 def nsl():
     nsldata = arff.loadarff('data/nsl/KDDTrain+.arff')
-    nsldf = pd.DataFrame(nsldata[0])
-    print(nsldf.columns)
+    nsldf = pd.DataFrame(nsldata[0]).infer_objects()
+    print(nsldf.dtypes)
 
 ### Microsoft Malware
 def malware():
-    print('TODO')
+    with open('data/mal/datasample/0A32eTdBKayjCWhZqDOQ.asm', encoding = "ISO-8859-1") as f:
+        source = f.readlines()
+        hex_integer = Word(hexnums) + WordEnd() # use WordEnd to avoid parsing leading a-f of non-hex numbers as a hex
+        line = ".text:" + hex_integer + Optional((hex_integer*(1,))("instructions") + Word(alphas,alphanums)("opcode"))
+        for source_line in source:
+            result = line.parseString(source_line)
+            if "opcode" in result:
+                print(result.opcode, result.instructions.asList())
 
 if __name__ == '__main__':
     main()
