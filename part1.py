bfcaff95b602b20acc34d5e6993001f2210a8f6b
Tom Taylor <ttaylor3.142@gmail.com>
2021-06-22 13:55:44 +0100

    Fix crash

--- part1.py
+++ part1.py
@@ -19,13 +19,15 @@ def nsl():
 
 ### Microsoft Malware
 def malware():
-    with open('data/mal/datasample/0A32eTdBKayjCWhZqDOQ.asm', encoding = "ISO-8859-1") as f:
+    with open('data/mal/datasample/0A32eTdBKayjCWhZqDOQ.asm', encoding = 'ISO-8859-1') as f:
         source = f.readlines()
         hex_integer = Word(hexnums) + WordEnd() # use WordEnd to avoid parsing leading a-f of non-hex numbers as a hex
-        line = ".text:" + hex_integer + Optional((hex_integer*(1,))("instructions") + Word(alphas,alphanums)("opcode"))
+        line = '.text:' + hex_integer + Optional((hex_integer*(1,))('instructions') + Word(alphas,alphanums)('opcode'))
         for source_line in source:
+            if 'text' not in source_line:
+                break;
             result = line.parseString(source_line)
-            if "opcode" in result:
+            if 'opcode' in result:
                 print(result.opcode, result.instructions.asList())
 
 if __name__ == '__main__':
