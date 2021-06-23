12c389e23de4e09bf2696d84563bb2867fb251de
Tom Taylor <ttaylor3.142@gmail.com>
2021-06-23 15:42:01 +0100

    Add aimf malware processing

--- part1.py
+++ part1.py
@@ -6,6 +6,8 @@
 
 from pyparsing import Word, hexnums, WordEnd, Optional, alphas, alphanums
 from scipy.io import arff
+import collections
+import os
 import pandas as pd
 
 def main():
@@ -19,7 +21,24 @@ def nsl():
 
 ### Microsoft Malware
 def malware():
-    with open('data/mal/datasample/0A32eTdBKayjCWhZqDOQ.asm', encoding = 'ISO-8859-1') as f:
+    #train_set(aimf)
+    labels = pd.read_csv('data/mal/trainLabels.csv')
+    print(labels)
+
+def malware():
+    labels = pd.read_csv('data/mal/trainLabels.csv')
+    XY = pd.DataFrame()
+    #for entry in os.scandir('data/mal/train'):
+    for entry in os.scandir('data/mal/datasample'):
+        if entry.name.endswith('.asm'):
+            typeclass = labels[labels['Id'] == os.path.splitext(entry.name)[0]].iloc[0]['Class']
+            XY = XY.append({'class':typeclass}|aimf(entry), ignore_index=True)
+
+    print(XY)
+
+def aimf(entry):
+    aimf = dict([(i,0) for i in range(256)])
+    with open(entry.path, encoding = 'ISO-8859-1') as f:
         source = f.readlines()
         hex_integer = Word(hexnums) + WordEnd() # use WordEnd to avoid parsing leading a-f of non-hex numbers as a hex
         line = '.text:' + hex_integer + Optional((hex_integer*(1,))('instructions') + Word(alphas,alphanums)('opcode'))
@@ -28,7 +47,10 @@ def malware():
                 break;
             result = line.parseString(source_line)
             if 'opcode' in result:
-                print(result.opcode, result.instructions.asList())
+                aimf[int(result.instructions.asList()[0], 16)] += 1
+
+    d = dict(collections.Counter(aimf))
+    return d
 
 if __name__ == '__main__':
     main()
