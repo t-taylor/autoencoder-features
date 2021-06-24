5af8ba6f1384a6c3bb00f89e66cc154d0bf21871
Tom Taylor <ttaylor3.142@gmail.com>
2021-06-24 10:57:47 +0100

    Add unigram representation

--- part1.py
+++ part1.py
@@ -7,6 +7,7 @@
 from pyparsing import Word, hexnums, WordEnd, Optional, alphas, alphanums
 from scipy.io import arff
 import collections
+import math
 import os
 import pandas as pd
 
@@ -20,21 +21,16 @@ def nsl():
     print(nsldf.dtypes)
 
 ### Microsoft Malware
-def malware():
-    #train_set(aimf)
-    labels = pd.read_csv('data/mal/trainLabels.csv')
-    print(labels)
-
-def malware():
-    labels = pd.read_csv('data/mal/trainLabels.csv')
-    XY = pd.DataFrame()
-    #for entry in os.scandir('data/mal/train'):
-    for entry in os.scandir('data/mal/datasample'):
-        if entry.name.endswith('.asm'):
-            typeclass = labels[labels['Id'] == os.path.splitext(entry.name)[0]].iloc[0]['Class']
-            XY = XY.append({'class':typeclass}|aimf(entry), ignore_index=True)
-
-    print(XY)
+# Classes
+# 1 Ramnit
+# 2 Lollipop
+# 3 Kelihos_ver3
+# 4 Vundo
+# 5 Simda
+# 6 Tracur
+# 7 Kelihos_ver1
+# 8 Obfuscator.ACY
+# 9 Gatak
 
 def aimf(entry):
     aimf = dict([(i,0) for i in range(256)])
@@ -51,6 +47,21 @@ def aimf(entry):
 
     d = dict(collections.Counter(aimf))
     return d
+
+def unigram(entry):
+    aimf_res = aimf(entry)
+    return {k:1 + math.log(v) if v != 0 else 0 for k, v in aimf_res.items()}
+
+def malware():
+    labels = pd.read_csv('data/mal/trainLabels.csv')
+    XY = pd.DataFrame()
+    #for entry in os.scandir('data/mal/train'):
+    for entry in os.scandir('data/mal/datasample'):
+        if entry.name.endswith('.asm'):
+            typeclass = labels[labels['Id'] == os.path.splitext(entry.name)[0]].iloc[0]['Class']
+            XY = XY.append({'class':typeclass}|unigram(entry), ignore_index=True)
+
+    print(XY)
 
 if __name__ == '__main__':
     main()
