6722034489bed3202c0c8f36d76c9fd7c9bccef8
Tom Taylor <tom@planccck.xyz>
2021-07-13 14:20:01 +0100

    results gen framework

--- part2.py
+++ part2.py
@@ -3,6 +3,11 @@
 # * dimensional reduction with variance threshold and PCA 
 # * all permutations with other ml algorithms
 
+### Workflow
+# * Step 1 dimensional reduction, Autoecoder, PCA with Autoecoder, PCA with variance threshold, or Nothing
+# * Step 2, Choice of ML algorithm
+# * Step 3, Calculate metrics
+
 from part1 import nsl_multiclass, malware_df
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.preprocessing import LabelEncoder
@@ -12,37 +17,63 @@ import tensorflow as tf
 import utils.ml as ml
 
 # A utility method to create a tf.data dataset from a Pandas Dataframe
-def df_to_dataset(dataframe, shuffle=True, batch_size=32):
-  dataframe = dataframe.copy()
-  labels = dataframe.pop('class')
-  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-  if shuffle:
-    ds = ds.shuffle(buffer_size=len(dataframe))
-  ds = ds.batch(batch_size)
-  return ds
-
-def get_category_encoding_layer(name, dataset, dtype, max_tokens=None):
-  # Create a StringLookup layer which will turn strings into integer indices
-  if dtype == 'string':
-    index = preprocessing.StringLookup(max_tokens=max_tokens)
-  else:
-    index = preprocessing.IntegerLookup(max_tokens=max_tokens)
-  # Prepare a Dataset that only yields our feature
-  feature_ds = dataset.map(lambda x, y: x[name])
-  # Learn the set of possible values and assign them a fixed integer index.
-  index.adapt(feature_ds)
-  # Create a Discretization for our integer indices.
-  encoder = preprocessing.CategoryEncoding(num_tokens=index.vocabulary_size())
-  # Apply one-hot encoding to our indices. The lambda function captures the
-  # layer so we can use them, or include them in the functional model later.
-  return lambda feature: encoder(index(feature))
-
 def test_model(inlen, outlen):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(inlen, input_dim=inlen, activation='relu'))
     model.add(tf.keras.layers.Dense(outlen, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return lambda: model
+
+def dimentional_reductions(train, test):
+  methods = ['autoencoder', 'pca_with_autoencoder', 'pca_with_varthresh', 'none']
+  outputs = {}
+  for method in methods:
+    if method == 'none':
+      outputs[method] = (train, test)
+    if method == 'pca_with_varthresh':
+      # TODO
+      outputs[method] = (train, test)
+    if method == 'autoencoder':
+      # TODO
+      outputs[method] = (train, test)
+    if method == 'pca_with_autoencoder':
+      # TODO
+      outputs[method] = (train, test)
+  return outputs
+
+def generate_models(train, test):
+  methods = ['bayesian', 'svm', 'neural_net', 'decision_tree']
+  outputs = {}
+  for method in methods:
+    if method == 'bayesian':
+      # TODO
+      outputs[method] == 'some_model'
+    if method == 'svm':
+      # TODO
+      outputs[method] == 'some_model'
+    if method == 'neural_net':
+      # TODO
+      outputs[method] == 'some_model'
+    if method == 'decision_tree':
+      # TODO
+      outputs[method] == 'some_model'
+
+def get_metrics(model, test):
+  metrics = ['accuracy', 'tpr', 'tnr', 'ppv']
+  output = {}
+  for metric in metrics:
+    if metric == 'accuracy':
+      # TODO
+      output[metric] = 'a'
+    if metric == 'tpr':
+      # TODO
+      output[metric] = 'a'
+    if metric == 'tnr':
+      # TODO
+      output[metric] = 'a'
+    if metric == 'ppv':
+      # TODO
+      output[metric] = 'a'
 
 def main():
     (train, test) = nsl_multiclass()
@@ -62,24 +93,13 @@ def main():
     (_, inlen) = X.shape
     (_, outlen) = dummy_y.shape
 
-    estimator = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=test_model(inlen, outlen), epochs=200, batch_size=5, verbose=0)
+    estimator = tf.keras.wrappers.scikit_learn.KerasClassifier(
+      build_fn=test_model(inlen, outlen), epochs=20, batch_size=2, verbose=2)
 
     kfold = KFold(n_splits=10, shuffle=True)
 
     results = cross_val_score(estimator, X, dummy_y, cv=kfold)
     print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-    #train_ds = df_to_dataset(train)
-    #test_ds = df_to_dataset(test)
-
-    #feature_cols = []
-    #for col in list(train.columns):
-    #    if 'class' not in col or 'difficulty' not in col:
-    #        feature_cols.append(tf.feature_column.numeric_column(col))
-
-    #model = ml.dense_network(feature_cols)
-
-    #model.fit(train_ds,
-    #      epochs=10)
 
 if __name__ == '__main__':
     main()
