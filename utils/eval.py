fffe096e3f420e4f536e7f560844312268dfe59c
Tom Taylor <tom@planccck.xyz>
2021-07-21 15:01:08 +0100

    Add basic metrics

--- utils/eval.py
+++ utils/eval.py
@@ -1,19 +1,14 @@
+import sklearn.metrics as met
+
 def get_metrics(model, X_test, Y_test):
     output = {}
 
-    n = X_test.shape[0]
     Y_pred = model.predict(X_test)
-    inccorrect = (Y_test != Y_pred).sum()
 
-    # Accuracy metric
-    output['accuracy'] = 1 - (inccorrect / n)
-
-    # TODO
-    output['tpr'] = 'a'
-    # TODO
-    output['tnr'] = 'a'
-    # TODO
-    output['ppv'] = 'a'
+    output['accuracy'] = met.accuracy_score(Y_test, Y_pred)
+    output['precision'] = met.precision_score(Y_test, Y_pred)
+    output['recall'] = met.recall_score(Y_test, Y_pred)
+    output['f1'] = met.f1_score(Y_test, Y_pred, average='weighted')
 
     return output
 
